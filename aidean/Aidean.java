e954fc2c86394a4a40a8585120b1968c64ef4fc5
tokutakeAyato <tokutake_ayato@yse-c.net>
2017-01-23 11:31:32 +0900

    add

--- aidean/Aidean.java
+++ aidean/Aidean.java
@@ -11,12 +11,24 @@ public class Aidean{
 
 class Aideaman {
 	JFrame frame;
+	JButton button;
+	JButton button2;
+	JPanel panel;
+
+
 
     public Aideaman(){
     	frame= new JFrame("Aloha");
     	frame.setLocation(400,250);
     	frame.setSize(300,300);
     	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    	button = new JButton("Aloha");
+    	button2 = new JButton("アロハ");
+
+    	panel = new JPanel();
+    	panel.add(button);
+    	panel.add(button2);
 
     }
 

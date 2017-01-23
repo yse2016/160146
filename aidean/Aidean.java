b1f3e270c445144483c92ab7dc13760afb8a5be2
tokutakeAyato <tokutake_ayato@yse-c.net>
2017-01-23 11:24:41 +0900

    add

--- aidean/Aidean.java
+++ aidean/Aidean.java
@@ -1,5 +1,23 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
 public class Aidean{
 	public static void main(String[]args){
-		System.out.println("Aloha");
+		Aideaman adm = new Aideaman();
 	}
+}
+
+class Aideaman {
+	JFrame frame;
+
+    public Aideaman(){
+    	frame= new JFrame("Aloha");
+    	frame.setLocation(400,250);
+    	frame.setSize(300,300);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
 }

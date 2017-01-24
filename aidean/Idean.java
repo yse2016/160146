a4abaef8a3a2777f2847633b8fd9e3eed69af723
tokutakeAyato <tokutake_ayato@yse-c.net>
2017-01-24 15:59:41 +0900

    add

--- aidean/Idean.java
+++ aidean/Idean.java
@@ -4,53 +4,106 @@ import javax.swing.*;
 import java.io.*;
 
 public class Idean{
-	public static void main(String[]args){
-		Ideaman im = new Ideaman();
-	}
+    public static void main(String[] args) {
+        IdeanMan idm = new IdeanMan();
+    }
 }
 
-class Ideaman {
-	JFrame frame;
-	JButton wordbtn;
-	JButton memobtn;
-	JPanel panel;
-	JTextField field;
-	JTextArea area;
-	JTextArea area2;
-    JScrollPane scrollPane;
 
 
-    public Ideaman(){
+class IdeanMan implements ActionListener{
+    JFrame frame;
+    JTextField idea_Name1;
+    JTextField idea_Name2;
+    JTextField file_Name;
+    JTextArea area;
+    JPanel field_Panel;
+    JButton btn_Idea;
+    JButton btn_Save;
+    JLabel label_Plus;
 
-    	frame= new JFrame("Aloha");
-    	frame.setLocation(500,500);
-    	frame.setSize(500,500);
-    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ public IdeanMan(){
 
-    	field =new JTextField(20);
-    	wordbtn = new JButton("単語を保存");
-    	memobtn = new JButton("メモを保存");
+        frame = new JFrame("Idean");
+        frame.setLocation( 600, 250 );
+        frame.setSize( 512, 275 );
 
-    	area =new JTextArea(10,10);
-    	area2= new JTextArea(20,20);
-    	area.setLineWrap(true);
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        idea_Name1 = new JTextField("ワード",20);
+        idea_Name2 = new JTextField("ワード",20);
 
-    	panel = new JPanel();
-    	panel.add(wordbtn);
-    	panel.add(memobtn);
-    	panel.add(field);
-    	panel.add(area);
-    	panel.add(area2);
-
-        scrollPane = new JScrollPane(area);
-
-        Container con =frame.getContentPane();
-        con.setLayout(new GridLayout(2,1));
-        con.add(panel);
-        con.add(scrollPane);
+        area = new JTextArea( 10, 30 );
 
 
-  	    frame.setVisible(true);
+        file_Name = new JTextField("ファイル名を入力.txt",15);
 
+        field_Panel = new JPanel();
+
+        btn_Idea = new JButton("単語2個表示");
+        btn_Idea.addActionListener(this);
+        btn_Idea.setActionCommand("idea");
+
+        btn_Save = new JButton("保存");
+        btn_Save.addActionListener(this);
+        btn_Save.setActionCommand("save");
+
+        label_Plus = new JLabel("+");
+
+
+        field_Panel.add( idea_Name1 );
+        field_Panel.add( label_Plus );
+        field_Panel.add( idea_Name2 );
+        field_Panel.add( btn_Idea );
+        field_Panel.add( area );
+        field_Panel.add( file_Name );
+        field_Panel.add( btn_Save );
+
+        Container con = frame.getContentPane();
+        con.setLayout( new GridLayout( 1, 1 ) );
+        con.add( field_Panel );
+
+           frame.setVisible(true);
+    }
+
+        public void actionPerformed( ActionEvent ae ){
+        String[] idea1 = {"アップルティ","レモンティ"};
+        String[] idea2 = {"タブレット","ウォークマン"};
+
+        String cmd = ae.getActionCommand();
+
+        if( cmd.equals("Idea") ){
+
+        } else if( cmd.equals("save") ){
+
+                String text_File_Name = file_Name.getText();
+
+                FileWriter fw = null;
+                PrintWriter pw = null;
+
+                try{
+                    fw = new FileWriter( text_File_Name );
+                    pw = new PrintWriter( fw );
+
+                    String data1 = idea_Name1.getText();
+                    String data2 = idea_Name2.getText();
+                    String data3 = area.getText();
+
+        pw.println( data1 +" + "+ data2 );
+        pw.println("↓");
+        pw.println( data3 );
+        } catch( IOException e ){
+            System.out.println("エラー");
+        } finally {
+        try{
+        fw.close();
+        pw.close();
+        } catch( IOException e ){
+            System.out.println("エラー");
+        }
+        }
+
+    }
+    }
 }
-}
+
+
